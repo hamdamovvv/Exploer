0eaa692c3f9c88ce0d80b24e27be8216896f3a05
hamdamovvv <akhmadkhamdamov000@gmail.com>
2023-10-20 16:17:06 +0600

    Version 1

---
+++ main.js
@@ -0,0 +1,14 @@
+const play = document.querySelector("button")
+const music = document.querySelector("#audio")
+let a = 1
+
+play.addEventListener('click', (e) => {
+    a++
+    e.preventDefault()
+    if (a % 2 === 0) {
+        music.play()
+    }
+    else {
+        music.pause()
+    }
+})
